241e6c5cc1a59b771c49433321917c2ce5ae1e0c
PetitPrince <code@hoangpham.ch>
2021-03-15 18:57:33 +0100

    Previz complete

---
+++ previz-crausaz-ete/data.js
@@ -0,0 +1,219 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-centre",
+      "name": "Centre",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.33858148993757453,
+          "pitch": 0.10357089246413942,
+          "rotation": 0,
+          "target": "3-bar-outdoor"
+        },
+        {
+          "yaw": -1.980298575997212,
+          "pitch": -0.07180003337818341,
+          "rotation": 0,
+          "target": "4-joli-coin"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-salon",
+      "name": "Salon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.61984655165229,
+          "pitch": 0.5542185812027629,
+          "rotation": 0,
+          "target": "2-jolie-vue"
+        },
+        {
+          "yaw": -0.5259845397894107,
+          "pitch": 0.138476797680692,
+          "rotation": 0,
+          "target": "0-centre"
+        },
+        {
+          "yaw": 3.1136089058692056,
+          "pitch": -0.18254970485640953,
+          "rotation": 0,
+          "target": "4-joli-coin"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-jolie-vue",
+      "name": "Jolie vue",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.19377404796201603,
+          "pitch": 0.18915628192081257,
+          "rotation": 0,
+          "target": "1-salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bar-outdoor",
+      "name": "Bar outdoor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7738345807979083,
+          "pitch": 0.2222882320302979,
+          "rotation": 0,
+          "target": "0-centre"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-joli-coin",
+      "name": "Joli coin",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2025501603173403,
+          "pitch": 0.1548007556979094,
+          "rotation": 0,
+          "target": "1-salon"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
--- previz-crausaz/data.js
+++ previz-crausaz/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-sam_147_0280",
-      "name": "SAM_147_0280",
+      "id": "0-terasse-1",
+      "name": "Terasse 1",
       "levels": [
         {
           "tileSize": 256,
@@ -22,31 +22,43 @@ var APP_DATA = {
           "size": 2048
         }
       ],
-      "faceSize": 1800,
+      "faceSize": 1944,
       "initialViewParameters": {
-        "yaw": 1.7108735539419655,
-        "pitch": 0.1603570273703383,
+        "pitch": 0,
+        "yaw": 0,
         "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": -2.426229568998215,
-          "pitch": 0.2462489817304956,
+          "yaw": -0.7596023704619252,
+          "pitch": 0.05557662227961657,
           "rotation": 0,
-          "target": "1-sam_147_0281"
+          "target": "8-bar"
         },
         {
-          "yaw": -0.10028174749386665,
-          "pitch": 0.24818363169104174,
+          "yaw": -0.14975549416086054,
+          "pitch": 0.2214287305985536,
           "rotation": 0,
-          "target": "3-sam_147_0283"
+          "target": "1-terasse-2"
+        },
+        {
+          "yaw": -3.0105015725536504,
+          "pitch": -0.13250421012388713,
+          "rotation": 0,
+          "target": "3-petit-coin-sympa"
+        },
+        {
+          "yaw": -2.646647411200238,
+          "pitch": 0.0024787889055080115,
+          "rotation": 0,
+          "target": "4-salle-en-cas-de-pluie-1"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-sam_147_0281",
-      "name": "SAM_147_0281",
+      "id": "1-terasse-2",
+      "name": "Terasse 2",
       "levels": [
         {
           "tileSize": 256,
@@ -66,31 +78,31 @@ var APP_DATA = {
           "size": 2048
         }
       ],
-      "faceSize": 1800,
+      "faceSize": 1944,
       "initialViewParameters": {
-        "yaw": 2.3603671583665697,
-        "pitch": 0.2064274726763209,
+        "pitch": 0,
+        "yaw": 0,
         "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": -0.6648214002750308,
-          "pitch": 0.23389383305606337,
+          "yaw": -1.4533053597151113,
+          "pitch": 0.0879177084984093,
           "rotation": 0,
-          "target": "0-sam_147_0280"
+          "target": "8-bar"
         },
         {
-          "yaw": 1.6634030098332317,
-          "pitch": 0.8631920470752519,
+          "yaw": 0,
+          "pitch": 0,
           "rotation": 0,
-          "target": "2-sam_147_0282"
+          "target": "2-terasse-3"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-sam_147_0282",
-      "name": "SAM_147_0282",
+      "id": "2-terasse-3",
+      "name": "Terasse 3",
       "levels": [
         {
           "tileSize": 256,
@@ -110,25 +122,25 @@ var APP_DATA = {
           "size": 2048
         }
       ],
-      "faceSize": 1800,
+      "faceSize": 1944,
       "initialViewParameters": {
-        "yaw": 2.816343408471517,
-        "pitch": 0.22873815066228076,
+        "pitch": 0,
+        "yaw": 0,
         "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": 0.1166150807981019,
-          "pitch": 0.3587976813039937,
+          "yaw": -1.8648814965117673,
+          "pitch": 0.10864338749910196,
           "rotation": 0,
-          "target": "1-sam_147_0281"
+          "target": "8-bar"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "3-sam_147_0283",
-      "name": "SAM_147_0283",
+      "id": "3-petit-coin-sympa",
+      "name": "Petit coin sympa",
       "levels": [
         {
           "tileSize": 256,
@@ -148,18 +160,268 @@ var APP_DATA = {
           "size": 2048
         }
       ],
-      "faceSize": 1800,
+      "faceSize": 1944,
       "initialViewParameters": {
-        "yaw": -0.05686847059754996,
-        "pitch": 0.06659230537509586,
+        "pitch": 0,
+        "yaw": 0,
         "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": 2.661288777426921,
-          "pitch": 0.14937905241267302,
+          "yaw": 1.8237584349585374,
+          "pitch": 0.1697895284438644,
           "rotation": 0,
-          "target": "1-sam_147_0281"
+          "target": "0-terasse-1"
+        },
+        {
+          "yaw": 1.4771124437027625,
+          "pitch": 0.13839373465090787,
+          "rotation": 0,
+          "target": "4-salle-en-cas-de-pluie-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-salle-en-cas-de-pluie-1",
+      "name": "Salle en cas de pluie 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7440341287006511,
+          "pitch": 0.15361783477003144,
+          "rotation": 0,
+          "target": "0-terasse-1"
+        },
+        {
+          "yaw": -3.1276739112665126,
+          "pitch": 0.07921347273673263,
+          "rotation": 0,
+          "target": "5-salle-en-cas-de-pluie-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-salle-en-cas-de-pluie-2",
+      "name": "Salle en cas de pluie 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.715995348788505,
+          "pitch": 0.09566097513352645,
+          "rotation": 0,
+          "target": "4-salle-en-cas-de-pluie-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-interieur-1",
+      "name": "Interieur 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.076087812453621,
+          "pitch": -0.04488810322188996,
+          "rotation": 0,
+          "target": "7-interieur-2"
+        },
+        {
+          "yaw": 0.8354467956458702,
+          "pitch": 0.39428474245740475,
+          "rotation": 0,
+          "target": "8-bar"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-interieur-2",
+      "name": "Interieur 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.092033700523272,
+          "pitch": 0.040045126815742904,
+          "rotation": 0,
+          "target": "6-interieur-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-bar",
+      "name": "Bar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9545525324417614,
+          "pitch": 0.12285627699311163,
+          "rotation": 0,
+          "target": "1-terasse-2"
+        },
+        {
+          "yaw": -0.12466637201889341,
+          "pitch": 0.005491164620151423,
+          "rotation": 0,
+          "target": "9-entrepot"
+        },
+        {
+          "yaw": 2.087302546009198,
+          "pitch": 0.00031289376266485647,
+          "rotation": 0,
+          "target": "6-interieur-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-entrepot",
+      "name": "Entrepot",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.33623637130209616,
+          "pitch": 0.19039474224714148,
+          "rotation": 0,
+          "target": "8-bar"
         }
       ],
       "infoHotspots": []
@@ -168,8 +430,8 @@ var APP_DATA = {
   "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
-    "autorotateEnabled": false,
+    "autorotateEnabled": true,
     "fullscreenButton": true,
-    "viewControlButtons": true
+    "viewControlButtons": false
   }
 };
